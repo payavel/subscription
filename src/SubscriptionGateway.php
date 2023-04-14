519cb9bff278a2e2309ea8292a6d78c4bfdc28b1
Robert Kujawa <robert.kujawa@proton.me>
2023-04-14 15:50:54 -0600

    Define the subscription gateway.

---
+++ src/SubscriptionGateway.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Payavel\Subscription;
+
+use Payavel\Subscription\Contracts\SubscriptionRequestor;
+
+class SubscriptionGateway extends SubscriptionService implements SubscriptionRequestor
+{
+    public function plans($subscriptionProduct = null)
+    {
+
+    }
+
+    public function listAgreements($subscriber)
+    {
+
+    }
+
+    public function getAgreement($subscription)
+    {
+
+    }
+
+    public function createAgreement($subscriber, $data)
+    {
+
+    }
+
+    public function updateAgreement($subscription, $data)
+    {
+
+    }
+
+    public function cancelAgreement($subscription)
+    {
+
+    }
+
+    public function activateAgreement($subscription)
+    {
+
+    }
+}
