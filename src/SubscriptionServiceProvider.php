7e98b48239cab717fc3cb299c12b9e4367b349b8
Robert Kujawa <robert.kujawa@proton.me>
2023-04-14 16:41:08 -0600

    Register singleton

--- src/SubscriptionServiceProvider.php
+++ src/SubscriptionServiceProvider.php
@@ -12,15 +12,19 @@ class SubscriptionServiceProvider extends ServiceProvider
             __DIR__ . '/stubs/config-publish.stub' => config_path('subscription.php'),
         ], 'payavel-config');
 
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-
         $this->publishes([
             __DIR__ . '/database/migrations/2023_01_01_000000_create_base_subscription_tables.php' => database_path('migrations/2023_01_01_000000_create_base_subscription_tables.php'),
         ], 'payavel-migrations');
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 
     public function register()
     {
+        $this->app->singleton(SubscriptionGateway::class, function ($app) {
+            return new SubscriptionGateway();
+        });
+
         $this->mergeConfigFrom(
             __DIR__ . '/config/subscription.php',
             'subscription'
