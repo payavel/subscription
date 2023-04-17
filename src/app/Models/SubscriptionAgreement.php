3d4c7d0fa0e7eeb3edc08b007f46aa749757424a
Carlos R <carlos.ruizvelasco2917@alumnos.udg.mx>
2023-04-17 12:37:46 -0600

    Define models for Subscription package.

---
+++ src/app/Models/SubscriptionAgreement.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionAgreement extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]|bool
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the Provider the Agreement method belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function provider()
+    {
+        return $this->belongsTo(SubscriptionProvider::class);
+    }
+
+    /**
+     * Get the Provider the Agreement method belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function plan()
+    {
+        return $this->belongsTo(SubscriptionPlan::class);
+    }
+}
