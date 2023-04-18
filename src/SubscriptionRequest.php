4fd2af12b8f90b5b5b972c2400436f6eb80b4a57
Robert Kujawa <robert.kujawa@proton.me>
2023-04-18 09:09:41 -0600

    Define SubscriptionRequest

--- src/SubscriptionRequest.php
+++ src/SubscriptionRequest.php
@@ -2,7 +2,39 @@
 
 namespace Payavel\Subscription;
 
-abstract class SubscriptionRequest
-{
+use Payavel\Checkout\Contracts\Providable;
+use Payavel\Subscription\Contracts\SubscriptionRequestor;
+use Payavel\Subscription\Traits\SubscriptionRequests;
 
+abstract class SubscriptionRequest implements SubscriptionRequestor
+{
+    use SubscriptionRequests;
+
+    /**
+     * The subscription provider.
+     *
+     * @var \Payavel\Checkout\Contracts\Providable
+     */
+    protected $provider;
+
+    /**
+     * @param  \Payavel\Checkout\Contracts\Providable $provider
+     * @param  \Payavel\Checkout\Contracts\Merchantable $merchant
+     */
+    public function __construct(Providable $provider)
+    {
+        $this->provider = $provider;
+
+        $this->setUp();
+    }
+
+    /**
+     * Set up the request.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        //
+    }
 }
