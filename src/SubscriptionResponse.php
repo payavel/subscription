c8b518647d29bea0f657e30e287b023a8922c25c
Robert Kujawa <robert.kujawa@proton.me>
2023-04-18 06:41:13 -0600

    Define the Subscription Response

--- src/SubscriptionResponse.php
+++ src/SubscriptionResponse.php
@@ -2,7 +2,193 @@
 
 namespace Payavel\Subscription;
 
-abstract class SubscriptionResponse
-{
+use Payavel\Checkout\Contracts\Providable;
+use Payavel\Checkout\Traits\SimulateAttributes;
+use Payavel\Subscription\Traits\SubscriptionResponses;
+use Payavel\Subscription\Contracts\SubscriptionResponder;
 
+abstract class SubscriptionResponse implements SubscriptionResponder
+{
+    use SimulateAttributes,
+        SubscriptionResponses;
+
+    /**
+     * Statuses in this array are considered successful.
+     *
+     * @var array
+     */
+    protected $successStatuses = [
+        // TODO: Define subscription statuses.
+    ];
+
+    /**
+     * Customize the response method names for your requests.
+     *
+     * @var array
+     */
+    protected $responseMethods = [];
+
+    /**
+     * The provider's raw response.
+     *
+     * @var mixed
+     */
+    protected $rawResponse;
+
+    /**
+     * Additional information needed to format the response.
+     *
+     * @var mixed
+     */
+    protected $additionalInformation;
+
+    /**
+     * The request method that returned this response.
+     *
+     * @var string
+     */
+    public $requestMethod;
+
+    /**
+     * The provider that the $request was made towards.
+     *
+     * @var \Payavel\Checkout\Contracts\Providable
+     */
+    public $provider;
+
+    /**
+     * The expected formatted data based on the $request.
+     *
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * @param mixed $rawResponse
+     * @param mixed $additionalInformation
+     */
+    public function __construct($rawResponse, $additionalInformation = null)
+    {
+        $this->rawResponse = $rawResponse;
+        $this->additionalInformation = $additionalInformation;
+    }
+
+    /**
+     * Configure the response based on the request.
+     *
+     * @param string $requestMethod
+     * @param \Payavel\Checkout\Contracts\Providable $provider
+     * @return void
+     */
+    public function configure(string $requestMethod, Providable $provider)
+    {
+        $this->requestMethod = $requestMethod;
+        $this->provider = $provider;
+    }
+
+    /**
+     * Get the provider's raw response.
+     *
+     * @return mixed
+     */
+    public function getRawResponse()
+    {
+        return $this->rawResponse;
+    }
+
+    /**
+     * Alias for the getRawResponse function.
+     *
+     * @return mixed
+     */
+    public function getRaw()
+    {
+        return $this->getRawResponse();
+    }
+
+    /**
+     * Verify whether the request should be considered successful.
+     *
+     * @return bool
+     */
+    public function isSuccessful()
+    {
+        return in_array($this->getStatusCode(), $this->successStatuses);
+    }
+
+    /**
+     * Verify whether the request should be considered a failure.
+     *
+     * @return bool
+     */
+    public function isNotSuccessful()
+    {
+        return ! $this->isSuccessful();
+    }
+
+    /**
+     * Determines the status code based on the request's raw response.
+     *
+     * @return int
+     */
+    public abstract function getStatusCode();
+
+    /**
+     * Get a string representation of the response's status.
+     *
+     * @return string|null
+     */
+    public function getStatus()
+    {
+        // TODO: Define subscription statuses.
+    }
+
+    /**
+     * Get a description of the response's status.
+     *
+     * @return string|null
+     */
+    public function getMessage()
+    {
+        // TODO: Define subscription statuses.
+    }
+
+    /**
+     * Get the formatted details based on the request that was made.
+     *
+     * @return array|mixed
+     *
+     * @throws \RuntimeException
+     */
+    public function getData()
+    {
+        if (! isset($this->data)) {
+            $this->data = $this->{$this->getResponseMethod()}();
+        }
+
+        return $this->data;
+    }
+
+    /**
+     * Get the response method that should be used to get the response's data.
+     *
+     * @return string
+     */
+    protected function getResponseMethod()
+    {
+        if (isset($this->requestMethod)) {
+            if (
+                array_key_exists($this->requestMethod, $this->responseMethods) &&
+                method_exists($this, $method = $this->responseMethods[$this->requestMethod])
+            ) {
+                return $method;
+            }
+
+            if (method_exists($this, $method = "{$this->requestMethod}Response")) {
+                return $method;
+            }
+        }
+
+        return 'response';
+    }
 }
