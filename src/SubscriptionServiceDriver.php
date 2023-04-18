f6d6a4d06b85aeba7f2e1176cc4e57a38bd5bb1c
Robert Kujawa <robert.kujawa@proton.me>
2023-04-18 12:36:31 -0600

    Define SubscriptionServiceDriver abstractions

--- src/SubscriptionServiceDriver.php
+++ src/SubscriptionServiceDriver.php
@@ -4,5 +4,29 @@ namespace Payavel\Subscription;
 
 abstract class SubscriptionServiceDriver
 {
+    /**
+     * Resolve the providable instance.
+     *
+     * @param \Payavel\Checkout\Contracts\Providable|string|int $provider
+     * @return \Payavel\Checkout\Contracts\Providable|null
+     */
+    abstract public function resolveProvider($provider);
 
+    /**
+     * Get the default providable identifier.
+     *
+     * @return string|int
+     */
+    public function getDefaultProvider()
+    {
+        return config('payment.defaults.provider');
+    }
+
+    /**
+     * Resolve the gateway class.
+     *
+     * @param \Payavel\Checkout\Contracts\Providable $provider
+     * @return string
+     */
+    abstract public function resolveGatewayClass($provider);
 }
