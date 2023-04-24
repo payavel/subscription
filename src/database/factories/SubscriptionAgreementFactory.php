fd0b998a9aa078c3f2c94a8b368fe2931cc29d9f
Robert Kujawa <robert.kujawa@proton.me>
2023-04-24 12:32:20 -0600

    Update SubscriptionAgreementFactory.php

--- src/database/factories/SubscriptionAgreementFactory.php
+++ src/database/factories/SubscriptionAgreementFactory.php
@@ -5,6 +5,8 @@ namespace Payavel\Subscription\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Payavel\Subscription\Models\SubscriptionPlan;
 use Payavel\Checkout\Models\PaymentMethod;
+use Payavel\Subscription\Models\SubscriptionAgreement;
+use Payavel\Subscription\SubscriptionStatus;
 
 class SubscriptionAgreementFactory extends Factory
 {
@@ -17,12 +19,31 @@ class SubscriptionAgreementFactory extends Factory
     {
         return [
             'reference' => $this->faker->uuid(),
-            'subscription_plan_id' => SubscriptionPlan::factory()->create()->id,
             'primary_payment_method_id' => PaymentMethod::factory()->create()->id,
-            'backup_payment_method_id' => PaymentMethod::factory()->create()->id,
             'start_date' => $this->faker->date(),
             'next_billing_date' => $this->faker->date(),
-            'status' => $this->faker->randomElement([0, 1, 2]),
+            'status' => $this->faker->randomElement([
+                SubscriptionStatus::ACTIVE_AUTO_RENEW_ON,
+                SubscriptionStatus::ACTIVE_AUTO_RENEW_OFF,
+            ]),
         ];
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterMaking(function (SubscriptionAgreement $subscriptionAgreement) {
+            if (is_null($subscriptionAgreement->subscription_plan_id)) {
+                $subscriptionPlan = SubscriptionPlan::inRandomOrder()->firstOr(function () {
+                    return SubscriptionPlan::factory()->create();
+                });
+
+                $subscriptionAgreement->subscription_plan_id = $subscriptionPlan->id;
+            }
+        });
     }
 }
