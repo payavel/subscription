910d34bebfe5fd3708ae548725ed9ef0ee1ae878
Robert Kujawa <robert.kujawa@proton.me>
2023-04-24 15:44:15 -0600

    Fix legacy faker format

--- src/database/factories/SubscriptionProductFactory.php
+++ src/database/factories/SubscriptionProductFactory.php
@@ -25,8 +25,8 @@ class SubscriptionProductFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word,
-            'description' => $this->faker->sentence,
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
         ];
     }
 }
