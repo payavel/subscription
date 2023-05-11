dc2803fa4773ac356957c62cec7041cffee99680
Carlos R <carlos.ruizvelasco2917@alumnos.udg.mx>
2023-05-11 09:44:28 -0600

    Add ConfiguresGateway trait to subscription package

---
+++ src/Models/Traits/ConfiguresSubscriptionGateway.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Payavel\Subscription\Models\Traits;
+
+use Payavel\Subscription\SubscriptionGateway;
+
+trait ConfiguresSubscriptionGateway
+{
+    /**
+     * The subscription's pre-configured gateway.
+     *
+     * @var \Payavel\Subscription\SubscriptionGateway
+     */
+    private $subscriptionGateway;
+
+    /**
+     * Retrieve the subscription's configured gateway.
+     *
+     * @return \Payavel\Subscription\SubscriptionGateway
+     */
+    public function getGatewayAttribute()
+    {
+        if (! isset($this->subscriptionGateway)) {
+            $this->subscriptionGateway = (new SubscriptionGateway);
+        }
+
+        return $this->subscriptionGateway;
+    }
+}
