5f9983448842520c97fdfb057b587678ce77a6b6
Carlos R <carlos.ruizvelasco2917@alumnos.udg.mx>
2023-05-24 12:46:41 -0600

    Configure SubscriptionGateway by its provider

--- src/Models/Traits/ConfiguresSubscriptionGateway.php
+++ src/Models/Traits/ConfiguresSubscriptionGateway.php
@@ -21,7 +21,8 @@ trait ConfiguresSubscriptionGateway
     public function getGatewayAttribute()
     {
         if (! isset($this->subscriptionGateway)) {
-            $this->subscriptionGateway = (new SubscriptionGateway);
+            $this->subscriptionGateway = (new SubscriptionGateway)
+                ->provider($this->provider_id);;
         }
 
         return $this->subscriptionGateway;
